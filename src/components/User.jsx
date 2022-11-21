2233a05cd7b97e6fe1109771057e13759ee43744
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-11-21 09:34:18 +0000

    use state

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,26 +1,16 @@
-import React, { useId } from 'react'
+import React, { useState } from 'react'
 
 export default function User() {
-    const users = ["anish","kumar","test"];
-    
-    // example for forEach instead of map method
-    const newArray = [];
-    users.forEach((user)=>{
-        newArray.push(<h1>{user}</h1>);
-    })
-
-    // so instead of creating forEach we can use map method its ultimately return the new array and which we can print
+    const [users,setUsers] = useState([]);
+    const addMe = () =>{
+        console.log("adding more students..");
+        setUsers(users=>[...users,"anish"]);
+    }
+  
   return (
     <div>
-    {/* {users.map((user,index)=> <h1 key={index}>{index+1} {user}</h1>)} */}
-    
-    {/* use of forEach */}
-    {/* 1st way */}
-    {newArray[0]}
-    {newArray[1]}
-    {newArray[2]}
-    {/* 2nd way */}
-    {newArray}
+    <button onClick={addMe}>Click me to add</button>
+    {users.map((user,index)=> <h1 key={index}>{index+1} {user}</h1>)}
     </div>
   )
 }
