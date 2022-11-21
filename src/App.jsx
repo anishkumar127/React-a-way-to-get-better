1b77b0fd484b0c2c4109622f1a120c89a1309200
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-11-21 07:58:44 +0000

    pass event function as a props

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 function Button(props) {
-  const { color ,children, style} = props;
-  return <button  style={style}> {children}</button>;
+  const { color ,children, style,handler} = props;
+  
+  return <button  style={style} onClick={handler}> {children}</button>;
 }
 
 function App() {
@@ -12,10 +13,16 @@ function App() {
     fontSize :"20px",
     backgroundColor:"white"
   }
+  const loginButtonHandler = () =>{
+    console.log("Login");
+  }
+  const logoutButtonHandler = () =>{
+    console.log("Logout");
+  }
   return (
     <>
-    <Button  style={style}> <p> click here  </p> </Button>
-    <Button style={style}>Login </Button>
+    <Button handler={logoutButtonHandler}  style={style}> <p> click here  </p> </Button>
+    <Button handler={loginButtonHandler} style={style}>Login </Button>
   </>
   );
 }
