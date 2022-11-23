6fd726c221a0583a3f38be9f555fba90ef277e3e
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-11-23 09:27:37 +0000

    state change

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Modal from "./components/Modal";
+const userList =[
+  {
+    username:"anish",
+    status:"Pending"
+  },
+  {
+    username:"kumar",
+    status:"Pending"
+  }
+];
 function App() {
 
-  const clickHandler = (event) =>{
-    console.log(event);
-  }
-  const changeHandler= (event)=>{
-    console.log(event.target.value);
-  }
+const [users,setUsers] = useState(userList);
+
+const verifyHandler = (index) =>{
+const tmpUsers = [...users]; // copied object from array users;
+const TempUser = tmpUsers[index];
+const userObj = {...TempUser,status:"Active"} // updated object
+tmpUsers[index] = userObj;
+setUsers(tmpUsers);
+}
   return (
     <>
-    <input type="text" onChange={(event)=> {
-      return (event){
-        return changeHandler(event)
-      }
-    }}/>
+     <h1> hello everyone</h1>
+     {users.map((user,index)=>{
+      return(
+        <div key={index}>
+        <div>
+        <span style={{marginRight:"20px"}}>{user.username}</span>
+        <span style={{marginRight:"20px"}}>{user.status}</span>
+       </div>
+        <button onClick = {()=>verifyHandler(index)}>Verify</button>
+        </div>
+      )
+     })}
+   
 
-    
-     <button onClick={(event)=> clickHandler(event)}>Click me</button>
     </>
   );
 }
