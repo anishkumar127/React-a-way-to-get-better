872e457cc83a71136f193ccdc146c52ec474e128
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2022-11-23 11:23:20 +0000

    input data handling

--- src/components/PeopleForm.jsx
+++ src/components/PeopleForm.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 
 const PeopleForm = () => {
-  return (
-    <div>
-          <input type="text" placeholder='name'/>
-        <select >
-            <option value="default">Select</option>
-            <option value="active">Active</option>
-            <option value = "pending">pending</option>
-        </select>
-        <button>Add People</button> 
-    </div>
-  )
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const nameChangeHandler = (e) => {
+        const personName = e.target.value;
+        console.log(personName);
+    }
+    const statusChangeHanlder = (e) => {
+        const status = e.target.value;
+        console.log(status);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='name' onChange={nameChangeHandler} />
+                <select onChange={statusChangeHanlder}>
+                    <option value="Default">Select</option>
+                    <option value="Active">Active</option>
+                    <option value="Pending">pending</option>
+                </select>
+                <button type="submit">Add People</button>
+            </form>
+
+        </div>
+    )
 }
 
 export default PeopleForm
